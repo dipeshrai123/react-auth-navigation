7402a0c21768b03e77a9b39b6e45f60b6293e3c4
Dipesh Rai <raidipesh78@gmail.com>
2021-09-20 07:34:29 +0545

    // path for root route subPaths bug fix

--- src/core/Utils.ts
+++ src/core/Utils.ts
@@ -44,25 +44,25 @@ export const canUserAccess = (userRoles: Array<string>, path: string) => {
 };
 
 // Flattens the nested and sub paths
-export const getParsedPaths = (pathType: "subPaths" | "nestedPaths") => (
-  paths: any
-) => {
-  const getNestedArray = (pathObj: any): any => {
-    if (pathObj[pathType]) {
-      const routes = pathObj[pathType].map((route: any) => {
-        const modRoute = { ...route, path: `${pathObj.path}${route.path}` };
-        return getNestedArray(modRoute);
-      });
-      return [pathObj, ...routes].flat();
-    } else return pathObj;
+export const getParsedPaths =
+  (pathType: "subPaths" | "nestedPaths") => (paths: any) => {
+    const getNestedArray = (pathObj: any): any => {
+      if (pathObj[pathType]) {
+        const routes = pathObj[pathType].map((route: any) => {
+          const path = `${pathObj.path}${route.path}`.replace("//", "/");
+          const modRoute = { ...route, path };
+          return getNestedArray(modRoute);
+        });
+        return [pathObj, ...routes].flat();
+      } else return pathObj;
+    };
+
+    const allPaths = paths.map((path: any) => {
+      return getNestedArray(path);
+    });
+
+    return allPaths.flat();
   };
-
-  const allPaths = paths.map((path: any) => {
-    return getNestedArray(path);
-  });
-
-  return allPaths.flat();
-};
 
 // Re-order flatten arrays on the basis of path length descending wise to
 // solve problem of not exact nestedpaths
